0a69a29c75189f60c53ffaccd2670d494fc60e91
dingzhiwen <dingzw@ibaodashi.com>
2019-01-31 13:43:45 +0800

    all header

---
+++ SMRBaseCore/SMRBaseCore.h
@@ -0,0 +1,37 @@
+//
+//  SMRBaseCore.h
+//  SMRBaseCoreDemo
+//
+//  Created by 丁治文 on 2019/1/31.
+//  Copyright © 2019 sumrise. All rights reserved.
+//
+
+#ifndef SMRBaseCore_h
+#define SMRBaseCore_h
+
+/// SMRCoreKit
+#import "SMRDB.h"
+#import "SMRLifecycle.h"
+#import "SMRRouter.h"
+#import "SMRModel.h"
+#import "SMRDebug.h"
+#import "SMRNetwork.h"
+#import "NSError+SMRError.h"
+#import "SMRRanker.h"
+#import "SMRLayout.h"
+
+/// SMRUIKit
+#import "SMRController.h"
+#import "SMRAdapter.h"
+#import "SMRNavigationView.h"
+#import "SMRTableAssistant.h"
+#import "UIView+SMRResponder.h"
+#import "UIImage+SMR.h"
+#import "UIButton+SMR.h"
+
+/// SMRUtilsKit
+#import "SMRPhoneInfo.h"
+#import "SMRKeyChainManager.h"
+#import "SMRUDManager.h"
+
+#endif /* SMRBaseCore_h */
