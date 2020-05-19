24ed1ce9eff0e05a6b5947a62c692660933c5e7b
dingzhiwen <dingzw@ibaodashi.com>
2020-05-19 10:55:11 +0800

    cryptor

--- SMRBaseCore/SMRCoreKit/SMRCryptor/SMRCryptor.m
+++ SMRBaseCore/SMRCoreKit/SMRCryptor/SMRCryptor.m
@@ -463,34 +463,17 @@
     if (!self.length) {
         return nil;
     }
-    
-    static const unsigned char HexDecodeChars[] = {
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 1,       //49
-        2, 3, 4, 5, 6, 7, 8, 9, 0, 0,       //59
-        0, 0, 0, 0, 0, 10, 11, 12, 13, 14,
-        15, 0, 0, 0, 0, 0, 0, 0, 0, 0,      //79
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 10, 11, 12,    //99
-        13, 14, 15
-    };
-    
-    // convert data(NSString) to CString
-    const char *source = [self cStringUsingEncoding:NSUTF8StringEncoding];
-    // malloc buffer
     unsigned char *buffer;
-    NSUInteger length = strlen(source) / 2;
+    NSUInteger length = self.length/2;
     buffer = malloc(length);
     for (NSUInteger index = 0; index < length; index++) {
-        buffer[index] = (HexDecodeChars[source[index * 2]] << 4) + (HexDecodeChars[source[index * 2 + 1]]);
+        NSScanner *scanner = [NSScanner scannerWithString:[self substringWithRange:NSMakeRange(index*2, 2)]];
+        unsigned int hex = 0;
+        [scanner scanHexInt:&hex];
+        buffer[index] = hex;
     }
-    // init result NSData
     NSData *result = [NSData dataWithBytes:buffer length:length];
     free(buffer);
-    source = nil;
     return  result;
 }
 
